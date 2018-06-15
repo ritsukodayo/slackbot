f3d483ba4ca915004a2be00be18fe14fecf53f96
rika <rika_21@icloud.com>
2018-06-16 00:44:03 +0900

    "createnew"

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
-from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+from slackbot.bot import default_reply
+import urllib.request
+from bs4 import BeautifulSoup
+import requests
+
+# 該当する応答がない場合に反応するデコーダ
 # randomモジュールを読み込んでください
 import random
 # @respond_to('string')     bot宛のメッセージ
@@ -23,20 +28,8 @@ import random
 
 @default_reply
 def default_hadler(message):
-     message.reply(random.choice(hoi))
+    message.reply('そうなんだ')
 
-hoi = [
-    'へ〜何言ってるかわからないんだけど',
-    'そうなんだ、なんて言ってるかわからないけどね〜',
-    'はいはい、どういう意味かわからないです'
-    'は〜い、言っていることがわからないな〜'
-    'ふ〜ん'
-]
-
-
-@respond_to('名前')
-def mention_func(message):
-    message.reply('tsukuba+aiと言います。ビックデータから学習して,' + name + '適した選択をしてあげます') # メンション
 
 @respond_to(r'^name\s+\S.*')
 def name_is(message):
@@ -47,10 +40,6 @@ def name_is(message):
     msg = name + 'さんですね。\n```' + word + '```'
     message.reply(msg)
 
-@respond_to('天気')
-def mention_func(message):
-    message.reply('今日はいい天気だね〜、明日はきっともっといい天気になるよ') # メンション
-    message.reply(name + 'さんはどんな気分？')
 
 @respond_to('ご飯')
 def mention_func(message):
@@ -122,31 +111,11 @@ def mention_func(message):
     message.reply('僕も大変だよ') # メンション
     message.reply(name + 'はどう思う？')
 
-@respond_to('だよ')
-def default_hadler(message):
-     message.reply(random.choice(poi))
 
-poi = [
-    'ふ〜ん',
-    'へ〜',
-    'ホイホイ',
-    'は〜い',
-    'へ！'
-]
-
-@respond_to('たよ')
-def default_hadler(message):
-     message.reply(random.choice(poi))
-
-poi = [
-    'ふ〜ん',
-    'へ〜',
-    'ホイホイ',
-    'は〜い',
-    'へ！'
-]
-
-
+@respond_to('天気')
+def mention_func(message):
+    message.reply('いつの天気が知りたい？') # メンション
+    message.reply('tenkiのあとに半角開けて(今日,明日,明後日のどれかを入力してね)')
 
 @respond_to(r'じゃんけん\s+\S.*')
 def janken_is(message):
@@ -154,7 +123,6 @@ def janken_is(message):
     temp, word = text.split(None, 1)    # 設定する言葉を取り出す。tempには'name'が入る
     global ja     # 外で定義した変数の値を変えられるようにする
     ja = word     # デフォルトの返事を上書きする
-
 
     def validate(hand):
         if hand < 0 or hand > 2:
@@ -177,9 +145,7 @@ def janken_is(message):
         else:
             return '負け'
 
-
     player_hand = int(ja)
-
 
     if validate(player_hand):
         computer_hand = random.randint(0,2)
@@ -195,3 +161,38 @@ def janken_is(message):
         message.reply('結果は' + result + 'でした')
     else:
         message.reply('正しい数値を入力してください')
+
+@respond_to(r'tenki\s+\S.*')
+def tenki_is(message):
+    text = message.body['text']     # メッセージを取り出す
+    temp, word = text.split(None, 1)    # 設定する言葉を取り出す。tempには'name'が入る
+    global day     # 外で定義した変数の値を変えられるようにする
+    day = word     # デフォルトの返事を上書きする
+    msg = day + 'ですね。\n```' + word + '```'
+    message.reply(msg)
+
+    rssurl = "http://weather.livedoor.com/forecast/rss/area/080020.xml"
+
+    tenki = []
+    with urllib.request.urlopen(rssurl) as res:
+        xml = res.read()
+        soup = BeautifulSoup(xml, "html.parser")
+        for item in soup.find_all("item"):
+            description = item.find("description").string
+            if description.find("[ PR ]") == -1:
+                tenki.append(description)
+
+    if day == '今日':
+        a = 1
+        b = 2
+    elif day == '明日':
+        a = 2
+        b = 3
+    elif day == '明後日':
+        a = 3
+        b = 4
+
+    for i in range(a,b):
+        message_tenki = tenki[i]
+        payload = {'message': message_tenki}
+        message.reply(message_tenki)
